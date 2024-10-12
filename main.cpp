70c65f0900e3f68f9ab3d08ce297d97e439274df
4nteros <bogatovandrej345@gmail.com>
2024-10-12 08:34:06 +0200

    Lab2

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
+#include <cmath>
 #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double p, a, b, c;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    cout << "c = ";
+    cin >> c;
+    if (((a + b) <= c) || ((a + c) <= b) || ((c + b) <= a))
+    {
+        cout << "Not exist" << endl;
+    }
+    else 
+    {
+    p = (a + b + c) / 2;
+    cout << "p = " << p << endl;
+    cout << "S = " << sqrt(p*(p - a)*(p - b)*(p - c)) << endl;
+    }
+
+    return 0;
+}
